50b92805d07478dfcd9377af85fa35fd26b2ca6f
Umerkhx <uks38182@gmail.com>
2025-09-08 15:01:34 +0500

    navbar-changes

--- src/app/(Home)/Navbar.tsx
+++ src/app/(Home)/Navbar.tsx
@@ -1,89 +1,54 @@
 "use client"
-import { motion } from "framer-motion"
-import { ModeToggle } from '@/components/ui/mode-toggle'
-import Image from 'next/image'
-import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
-import { MainNav } from './MainNav'
-import { MobileNav } from './MobileNav'
+import { ModeToggle } from "@/components/ui/mode-toggle"
+import Image from "next/image"
+import Link from "next/link"
+import { MainNav } from "./MainNav"
+import { MobileNav } from "./MobileNav"
 import { useTranslation } from "@/hooks/useTranslation"
 import { usePathname } from "next/navigation"
 import { checkIsPathnameIsEqualToLang, urlSplitter } from "@/lib"
 import LanguageSwitcher from "@/components/ui/LanguageSwitcher"
 import { SidebarButton } from "@/components/sidebar-button"
-import { Skeleton } from "@/components/ui/skeleton"
 import { SearchButton } from "@/components/ui/search-button"
 
 function Navbar() {
-  const [isLoading, setIsLoading] = useState(true);
-  const { t, isLoading: translationLoading } = useTranslation("navigation")
+  const { t } = useTranslation("navigation")
   const pathname = usePathname()
   const currentLang = urlSplitter(pathname)
 
   const isLang = checkIsPathnameIsEqualToLang(currentLang)
 
-
-  useEffect(() => {
-    localStorage.setItem("loading", "true");
-    setIsLoading(true);
-
-    const timer = setTimeout(() => {
-      localStorage.setItem("loading", "false");
-      setIsLoading(false);
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  }, [])
-
-
   return (
-
-
-    <>
-
-      <motion.header
-        initial={{ y: -100 }}
-        animate={{ y: 0 }}
-        className="sticky top-0 z-[9999999999] border-b border-white/10 bg-[#003422] backdrop-blur dark:supports-[backdrop-filter]:bg-[#000717]"
-      >
-            {isLoading ? <div className='px-8 my-4'>
-            <Skeleton className="h-10 w-full " />
-          </div> : (
-        <div className="container mx-auto flex h-16 items-center justify-between px-4">
-      
-            <div className="flex items-center gap-2">
-              <Link href={isLang ? `/${currentLang}` : "/"} className="flex items-center gap-2">
-                <div className="relative h-8 w-8 overflow-hidden rounded-lg">
-                  <Image
-                    src="/global-salah-logo.webp"
-                    alt="globalsalah"
-                    width={260}
-                    height={230}
-                    priority
-                  />
-                </div>
-                <span className="text-lg font-semibold text-white md:block hidden"> {isLang ? t("navigation.title") : "Global Salah"} </span>
-              </Link>
+    <header className="sticky top-0 z-[9999999999] border-b border-white/10 bg-[#003422] backdrop-blur dark:supports-[backdrop-filter]:bg-[#000717]">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+        <div className="flex items-center gap-2">
+          <Link href={isLang ? `/${currentLang}` : "/"} className="flex items-center gap-2">
+            <div className="relative h-8 w-8 overflow-hidden rounded-lg">
+              <Image src="/global-salah-logo.webp" alt="globalsalah" width={260} height={230} priority />
             </div>
+            <span className="text-lg font-semibold text-white md:block hidden">
+              {isLang ? t("navigation.title") : "Global Salah"}
+            </span>
+          </Link>
+        </div>
 
-          <MainNav  />
+        <MainNav />
 
-          <div className="flex items-center md:gap-5 gap-2 ">
-            <SearchButton />
-            <div className="lg:flex gap-2 hidden">
-              <SidebarButton />
-            </div>
-            <LanguageSwitcher />
-            <ModeToggle />
-
-            <div className="md:hidden">
-              <MobileNav />
-            </div>
+        <div className="flex items-center md:gap-5 gap-2">
+          <SearchButton />
+          <div className="lg:flex gap-2 hidden">
+            <SidebarButton />
           </div>
-        </div>)}
-      </motion.header>
-    </>
+          <LanguageSwitcher />
+          <ModeToggle />
+
+          <div className="md:hidden">
+            <MobileNav />
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
-export default Navbar 
+export default Navbar
